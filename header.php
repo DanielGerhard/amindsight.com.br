def531d0dd118e2bb15bb0d92c49ed0168b00feb
Daniel Gerhard <vexogerhard@gmail.com>
2023-02-06 22:48:29 -0300

    VM SETADA COM WORPRESS FUNCIONANDO CARAIOOOOOOOOOOO

---
+++ header.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="<?php bloginfo('charset'); ?>" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>amindsight</title>
+	<link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_url'); ?>">
+	<script type="text/javascript" src="<?php bloginfo('template_directory'); ?>/js/script-name.js"></script>
+</head>
+
+<body>
+	<main id="front-page" class="site">
+
+		<div id="content" class="site-content">
+			<div id="primary" class="content-area">
+				<main id="main" class="site-main">
+
+
+
+
+					<!doctype html>
+					<html <?php language_attributes(); ?> <?php twentytwentyone_the_html_classes(); ?>>
