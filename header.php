e3cca5b79e4712b849e23f815c0d7253f3c96deb
Daniel Gerhard <vexogerhard@gmail.com>
2023-02-15 22:48:30 -0300

    atualizado com server

--- header.php
+++ header.php
@@ -6,19 +6,53 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>amindsight</title>
-	<link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_url'); ?>">
 	<script type="text/javascript" src="<?php bloginfo('template_directory'); ?>/js/script-name.js"></script>
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+	<link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_url'); ?>">
 </head>
 
 <body>
-	<main id="front-page" class="site">
 
-		<div id="content" class="site-content">
-			<div id="primary" class="content-area">
-				<main id="main" class="site-main">
+	<?php if (is_front_page()) { ?>
+		<div class="page-header">
+		<?php } // if(is_front_page())
+		?>
 
+		<header class="fixed-top">
+			<div class="container">
+				<div class="row justify-content-between">
 
+					<a href="/" class="home">&#8226;amindsight</a>
 
-
-					<!doctype html>
-					<html <?php language_attributes(); ?> <?php twentytwentyone_the_html_classes(); ?>>
+					<nav id="header">
+						<ul class="menu">
+							<a href="/">
+								<li class="item-menu">
+									Home
+								</li>
+							</a>
+							<a href="/gallery">
+								<li class="item-menu">
+									Gallery
+								</li>
+							</a>
+							<a href="/stories">
+								<li class="item-menu">
+									Stories
+								</li>
+							</a>
+							<a href="/games">
+								<li class="item-menu">
+									Games
+								</li>
+							</a>
+							<a href="/about">
+								<li class="item-menu">
+									About
+								</li>
+							</a>
+						</ul>
+					</nav>
+				</div>
+			</div>
+		</header>
--- index.php
+++ index.php
@@ -1,4 +1,22 @@
-<?php get_header() ?>
+<?php get_header(); ?>
 
-echo "hudisokhsuiao"; 
-<?php get_footer() ?>
+<img src="https://amindsight.pythonanywhere.com/static/css/img/CAPASITE/CAPASITE.jpg" alt="" class="background-image">
+
+<section id="banner">
+    <div class="container">
+        <div class="row">
+            <div class="col-5 offset-7">
+                <div class="titulo-banner">
+                    <h1>Sem arte, e sem amor,<br> nós somos apenas cascas vazias.</h1>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+</div>
+
+<section id="front-gallery" style="height:100px">
+    
+</section>
+
+<?php get_footer(); ?>
